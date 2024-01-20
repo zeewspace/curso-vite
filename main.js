3b45a0d640a2bbf6113e04c6c57a1b9807ed80c5
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-01-19 21:09:50 -0600

    4.2_json

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import './style.css'
 import logoZ from './ZeewLogo.webp'
+import jsinto from './data.json'
 
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Zeew Space</h1>
+    <p>${jsinto.user.name}</p>
+    <pre>${JSON.stringify(jsinto)}</pre>
   </div>
 `
 
