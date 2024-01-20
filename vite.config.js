b2ab00e922d5599a3300b43ce57455652615e465
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-01-19 21:39:53 -0600

    5.3_Multiples paginas

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(({ command, mode }) => {
   console.log({ command, mode, process: process.cwd() });
@@ -8,6 +9,14 @@ export default defineConfig(({ command, mode }) => {
   return {
     server: {
       port: env.VITE_PORT,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          about: resolve(__dirname, "about", "index.html"),
+        },
+      },
     },
   };
 });
